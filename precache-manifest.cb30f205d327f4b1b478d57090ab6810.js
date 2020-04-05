fa75d426796f0cdb919d79bca2a1a5f05c613d5e
hotaryuzaki <hotaru_edogawa@yahoo.com>
2020-04-05 22:07:06 +0700

    Updates

---
+++ precache-manifest.cb30f205d327f4b1b478d57090ab6810.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "685a5e5c680fa9c10c7e148c58bc7431",
+    "url": "/fabelio/index.html"
+  },
+  {
+    "revision": "920717631215095198e7",
+    "url": "/fabelio/static/css/main.a00c033f.chunk.css"
+  },
+  {
+    "revision": "8ec2899f4db5acaf0a4b",
+    "url": "/fabelio/static/js/2.62316857.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fabelio/static/js/2.62316857.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "920717631215095198e7",
+    "url": "/fabelio/static/js/main.fcdeb711.chunk.js"
+  },
+  {
+    "revision": "e363a9b5eacc761de2fa",
+    "url": "/fabelio/static/js/runtime-main.1a46433a.js"
+  }
+]);
